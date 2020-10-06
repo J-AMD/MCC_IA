689a5bf9120b8a9e4caf72d19d8aecabe378a44a
J-AMD <52175127+J-AMD@users.noreply.github.com>
2020-10-06 15:51:13 -0500

    Add files via upload
    Beginning with definition of matriz 15x100 to algorithms of traveler (evolutive algorithm)

---
+++ matriz.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+Script throught generated a matriz with dimension of 15x100
+This matriz is the first step to generate algorithms solutions
+"""
+import numpy as np
+
+print('Porfavor deefina el tamanio de la matriz a utilizar')
+print('Se recomienda el tamnio de 15X100\n')
+
+col = int(input('Ingrese el numero de columnas: ' ))
+row = int(input('Ingrese el numero de filas: '))
