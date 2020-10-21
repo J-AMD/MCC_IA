39494994f8597ad3987fb0cf65adf495a51e10ae
J-AMD <52175127+J-AMD@users.noreply.github.com>
2020-10-21 14:17:02 -0500

    Update matriz.py
    Create a graphic and draw the cities and the walk to follow

--- matriz.py
+++ matriz.py
@@ -1,12 +1,14 @@
 # -*- coding: utf-8 -*-
 """
 Spyder Editor
-
 Script throught generated a matriz with dimension of 15x100
 This matriz is the first step to generate algorithms solutions
 """
 import numpy as np
 import random
+import matplotlib
+import matplotlib.pyplot as plt
+
 
 print('Porfavor defina el tamanio de la matriz a utilizar')
 print('Valor por Default: 15X100')
@@ -17,10 +19,14 @@ if change == 1:
     col = int(input('Ingrese el numero de columnas: ' ))
 else:
     row = 100
-    col = 15
+    col = 14
     
 matriz = np.empty((row,col))
 val=[]
+
+city = [[1,1],[1,7],[2,3],[2,5],[3,2],[4,4],[5,1],[6,6],[7,3],[9,8],[10,5],[12,3],[13,1],[13,6]]
+listaX = []
+listaY = []
 
 for j in range(1,(col+1)):
     val.append(j)
@@ -28,3 +34,10 @@ for j in range(1,(col+1)):
 for i in range(row):
     matriz[i] = random.sample(val, col)
     print(matriz[i])
+
+for cit in city: #######################################create a grphic and draw the cities points
+    plt.plot(cit[0],cit[1], 'o')
+    plt.plot(listaX, listaY)
+
+plt.grid()
+plt.show()
